c74376e2b63cd517458d11792560103cea1fc8db
Eun-Ng <dhkdldms@gmail.com>
2022-07-23 02:18:47 +0900

    build structure

--- sparta-react-week1/src/App.js
+++ sparta-react-week1/src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const styles = {
+    border: '1px solid #eee',
+    padding: '20px',
+    margin: '30px auto',
+  };
+
+  return (
+    <div className='App'>
+      <div style={styles}>
+        <h1 style={{color: 'green'}}>안녕하세요!</h1>
+        <hr />
+        <p>이름을 입력해주세요.</p>
+        <input type='text' />
+      </div>
+    </div>
+  );
 }
 
 export default App;
